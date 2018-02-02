3fb40485bb4bc4ecd13d2ec275abf456392a2318
Pansyuk <panasyuk20anna@gmail.com>
2018-02-02 12:22:14 +0200

    landing page about IPhone X

---
+++ js/script.js
@@ -0,0 +1,3 @@
+$("body").on("click",".choose-model",function(){var e=$($(this).attr("href")).offset().top;return $("html,body").animate({scrollTop:e},800),!1});
+$(".containerForPolitic").fancybox({selector:'[data-fancybox="#politic"]',loop:!0}),$.fancybox.defaults.animationEffect="elastic";
+$.fancybox.defaults.animationEffect="fade";
